0e0e9b1563ca28236130d5220e23db77f0a33866
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-11-08 12:52:14 +0100

    Add in widget plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import vueCustomElement from 'vue-custom-element';
+
+Vue.use(vueCustomElement);
+
 Vue.config.productionTip = false
 
+Vue.customElement('vue-widget', App);
+/*
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+*/
